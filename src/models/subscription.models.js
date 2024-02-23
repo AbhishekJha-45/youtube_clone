1f864ccb615004d4bb5fa2367c3f843efd4ee395
AbhishekJha-45 <krjha36555@gmail.com>
2024-02-23 16:02:08 +0530

    Add subscription model

---
+++ src/models/subscription.models.js
@@ -0,0 +1,18 @@
+import { Schema } from "mongoose";
+
+const subscriptionSChema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    
+  },
+  { timestamps: true }
+);
+
+export const Subscription = model("Subscription", subscriptionSChema);
