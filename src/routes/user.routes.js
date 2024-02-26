5a01fd36fd727636a90070d531079cd0ecf81de3
AbhishekJha-45 <krjha36555@gmail.com>
2024-02-26 23:31:16 +0530

    Add new routes for user profile and account management

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import {
   changeCurrentUserPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
-
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
@@ -14,8 +19,16 @@ const router = Router();
 router
   .route("/register")
   .get((_, res) => res.json({ message: "Register route" }));
-router.route("/login").get((_, res) => res.json({ message: "get request for Login route recieved" }));
-router.route("/logout").get((_, res) => res.json({ message: "get request for Logout route recieved" }));
+router
+  .route("/login")
+  .get((_, res) =>
+    res.json({ message: "get request for Login route recieved" })
+  );
+router
+  .route("/logout")
+  .get((_, res) =>
+    res.json({ message: "get request for Logout route recieved" })
+  );
 //post routes
 router.route("/register").post(
   upload.fields([
@@ -29,5 +42,15 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJwt, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJwt, changeCurrentUserPassword);
+router.route("/current-user").post(verifyJwt, getCurrentUser);
+router.route("/update-account").patch(verifyJwt, updateAccountDetails);
+router
+  .route("/update-avatar")
+  .patch(verifyJwt, upload.single("avatar"), updateUserAvatar);
 
+router
+  .route("/update-cover-image")
+  .patch(verifyJwt, upload.single("coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJwt, getUserChannelProfile);
+router.route("/history").get(verifyJwt, getWatchHistory);
 export default router;
